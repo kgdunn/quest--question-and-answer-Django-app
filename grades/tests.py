49209e8fe0459d0f875657fede414295d0937802
Kevin Dunn <kevin.dunn@connectmv.com>
2013-03-10 18:24:17 -0400

    Working on grading part

--- grades/tests.py
+++ grades/tests.py
@@ -1,16 +1,35 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+#from django.test.client import Client
+#from django.core.urlresolvers import reverse
+#from person.models import Token, UserProfile, Timing
+#from django.conf import settings
+#from instructor.views import (create_question_template, render)
+#from course.models import Course
+#from question.models import (QTemplate, QActual, QSet)
+#import datetime
 
+import wingdbstub
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+#class Login_TestCases(TestCase):
+    #def test_login_before_start(self):
+        #c = Client(HTTP_USER_AGENT='ABC')  # enforce_csrf_checks=True,
+        #resp = c.get('/')#, {'username': 'john', 'password': 'smith'})
+
+        #self.assertEqual(resp.templates[1].name, 'base.html')
+        #self.assertEqual(resp.templates[0].name, 'person/sign-in-form.html')
+
+        #self.assertEquals(resp.status_code, 200)
+
+        ## Now sign-in with an email address
+
+        #settings.TESTING = True  # to prevent emails being sent
+        #n_tokens = len(Token.objects.all())
+        #user = UserProfile.objects.filter(role='Student')
+        #if user:
+            #email = user[0].user.email
+            #resp = c.post('/', {'user_mail_address': email})
+            #self.assertEquals(resp.status_code, 200)
+            #self.assertEqual(n_tokens+1, len(Token.objects.all()))
+            #self.assertEqual(resp.templates[0].name, 'person/sent-email.html')
+        #else:
+            #pass
--- grades/views.py
+++ grades/views.py
@@ -1,1 +1,46 @@
-# Create your views here.
+#import re
+#import csv
+#try:
+    #import simplejson as json
+#except ImportError:
+    #import json
+#import random
+#import logging
+#import hashlib
+#from collections import defaultdict
+
+#from django.conf import settings
+#from django.core.context_processors import csrf
+#from django.core.exceptions import ValidationError
+#from django.template import Context, Template, Library
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import (HttpResponse, render_to_response,
+                              RequestContext)
+
+
+# 3rd party imports
+
+# Our imports
+from question.models import (QTemplate, QActual, Inclusion)
+from question.views import validate_user
+
+@login_required                               # URL: ``grading-process-grades``
+def process_grades(request, course_code_slug, question_set_slug):
+    """
+    Processes the grades for a given course and question set. The main entry
+    point for initiating grading.
+
+    Displays a list of students and the grades achieved.
+    """
+    course = validate_user(request, course_code_slug, question_set_slug,
+                           admin=True)
+    if isinstance(course, HttpResponse):
+        return course
+    if isinstance(course, tuple):
+        course, qset = course
+
+    # Iterate through all questions by all students in the QSet
+    #quests = QActual.objects.filter(
+
+
+    return HttpResponse('All graded')
